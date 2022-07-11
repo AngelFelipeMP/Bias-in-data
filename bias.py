759c47a0ba2e560d1e87d31a26e02c57e01dd541
AngelFelipeMP <angel.magnossao@gmail.com>
2022-07-11 12:53:07 +0200

    "add script bias.py"

---
+++ bias.py
@@ -0,0 +1,24 @@
+import os
+import spacy
+
+CODE_PATH = os.getcwd()
+REPO_PATH = '/'.join(CODE_PATH.split('/')[0:-1])
+DATA_PATH = REPO_PATH + '/' + 'data'
+
+folderdata = 'SBW'
+filename = 'sbwce.clean.sample.txt'
+fileprocessed = filename[0:-4] + '.processed' + filename[-4:]
+
+#Load the spacy model
+nlp = spacy.load('es_core_news_sm', disable = ['parser','ner'])
+
+with open(DATA_PATH + '/' + folderdata + '/' + filename, 'r') as file:
+    with open(DATA_PATH + '/' + folderdata + '/' + fileprocessed, 'w') as newfile:
+        for line in file:
+            #Pass text to spacy and Do lowercase
+            doc = nlp(line.lower())
+            #Do lemmatization
+            join = " ".join([token.lemma_ for token in doc])
+            #Write to file "Processed"
+            newfile.write(join)
+
